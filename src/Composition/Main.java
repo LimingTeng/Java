e719d368c42092e8d829994210fb1503d7487733
Liming Teng <tengliming96@gmail.com>
2019-03-27 11:16:29 -0700

    Composition Bedroom

--- src/Composition/Main.java
+++ src/Composition/Main.java
@@ -18,7 +18,24 @@ public class Main {
 //        thePC.getMotherboard().loadProgram("Windows 1.0");
 //        thePC.getTheCase().pressPowerButton();
         thePC.powerUp();
+        System.out.println("-------------------------------------------------------");
 
-        //
+        // Bedroom, Bed, Ceiling, Lamp, Wall
+        Wall wall1 = new Wall("West");
+        Wall wall2 = new Wall("East");
+        Wall wall3 = new Wall("South");
+        Wall wall4 = new Wall("North");
+
+        Ceiling ceiling = new Ceiling(12, 55);
+
+        Bed bed = new Bed("Modern", 4, 3, 2, 1);
+
+        Lamp lamp = new Lamp("Classic", false, 75);
+
+        Bedroom bedroom = new Bedroom("Tims", wall1, wall2, wall3, wall4, ceiling, bed, lamp);
+        bedroom.makeBed();
+
+        bedroom.getLamp().turnOn();
+
     }
 }
