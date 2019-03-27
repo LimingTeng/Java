28832bee51306318297c2a67b274e008a2183dbc
Liming Teng <tengliming96@gmail.com>
2019-03-27 14:55:39 -0700

    Encapsulation Printer

--- src/Encapsulation/Main.java
+++ src/Encapsulation/Main.java
@@ -5,7 +5,18 @@ package Encapsulation;
  */
 public class Main {
     public static void main(String[] args) {
+        // EnhancedPlayer:
         EnhancedPlayer player = new EnhancedPlayer("Time", 50, "Sword");
         System.out.println("Initial health is " + player.getHealth());
+        System.out.println("-------------------------------------------------");
+
+        // Printer:
+        Printer printer = new Printer(50, true);
+        System.out.println("initial page count = " + printer.getPagePrinter());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer " + printer.getPagePrinter());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted + " new total print count for printer " + printer.getPagePrinter());
+
     }
 }
