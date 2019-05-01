edb5739aa09b706705e74c35767754d0659dfa16
Liming Teng <tengliming96@gmail.com>
2019-05-01 14:20:28 -0700

    add SearchTree

--- src/AbstractClass/ListItemMain.java
+++ src/AbstractClass/ListItemMain.java
@@ -16,5 +16,17 @@ public class ListItemMain {
         list.traverse(list.getRoot());
         list.removeItem(new Node("3"));
         list.traverse(list.getRoot());
+
+//        SearchTree tree = new SearchTree(null);
+//        tree.traverse(tree.getRoot());
+//
+//        String stringData = "5 7 3 9 8 2 1 0 4 6";
+//        String[] data = stringData.split(" ");
+//        for (String s : data) {
+//            tree.addItem(new Node(s));
+//        }
+//        tree.traverse(tree.getRoot());
+//        tree.removeItem(new Node("3"));
+//        tree.traverse(tree.getRoot());
     }
 }
